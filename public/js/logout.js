736b07e09f1289625589aeff5787aadaf550f658
Angela Kressin <angelakressin@gmail.com>
2018-03-28 22:20:03 -0400

    fixed logout routes

--- public/js/logout.js
+++ public/js/logout.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
   var authClient = new OktaAuth({
     url: 'https://dev-718531.oktapreview.com',
     clientId: '0oaeh5zb0oT1wYElN0h7'
-    redirectUri: '/'
+    redirectUri: 'https://guarded-garden-97987.herokuapp.com/'
   });
 
   authClient.signOut()
     .then(function() {
       console.log('successfully logged out');
-      window.location.href = '/';
+      window.location.href = 'https://guarded-garden-97987.herokuapp.com/';
     })
     .fail(function(err) {
       console.error(err);
-      window.location.href = '/';
+      window.location.href = 'https://guarded-garden-97987.herokuapp.com/';
     });
 
   localStorage.clear();
